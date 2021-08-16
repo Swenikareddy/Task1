cad8cd6ae4a3259b110b1c42f700daf9e8940c66
Swenikareddy <71818894+Swenikareddy@users.noreply.github.com>
2021-08-16 20:06:20 +0530

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  logo:'../assets/images/kudosbeta.png',
+  projects:['kudoswall','prokudoswall','kudoswall.edu']
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
