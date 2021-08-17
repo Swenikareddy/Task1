fa58e0bc7a2a85bb5837fa4978ebf0364e428efd
Swenikareddy <71818894+Swenikareddy@users.noreply.github.com>
2021-08-17 23:56:04 +0530

    created dev2 build

--- src/environments/environment.dev2.ts
+++ src/environments/environment.dev2.ts
@@ -1,5 +1,6 @@
 export const environment = {
     dev2: true,
+    production: false,
     logo:'../assets/images/kudosbeta.png',
     projects:['kudoswall.test','kudoswall.test2','kudoswall.test3'],
     
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  dev2: false,
   logo:'../assets/images/kudos.png',
   projects:['kudoswall.discover','kudoswall.blogs','kudoswall.org']
 };
